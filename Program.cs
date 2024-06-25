6b0fa15e82cd52ff65e58702511bfe96539b831b
Vitor Esbrolio Lima <74936968+VitorLima1@users.noreply.github.com>
2024-06-25 18:17:44 -0300

    Update Program.cs
    O programa principal deve criar instâncias de Nokia e Iphone, demonstrar a funcionalidade dos métodos e instalar aplicativos.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
-﻿using DesafioPOO.Models;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        Smartphone nokia = new Nokia(numero: "123456789", modelo: "Nokia 3310", imei: "111111111111111", memoria: 32);
+        Smartphone iphone = new Iphone(numero: "987654321", modelo: "iPhone 13", imei: "222222222222222", memoria: 128);
+
+        Console.WriteLine("Testando Nokia:");
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine("\nTestando iPhone:");
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
